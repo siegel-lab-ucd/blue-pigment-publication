30662d92e4564ed20a0057a5cb8b717c56a50344
siegel-lab-ucd <siegel.lab.ucd@gmail.com>
2020-12-23 14:20:23 -0800

    Add files via upload

---
+++ Buildtree_working.sh
@@ -0,0 +1,2 @@
+python2 graphlan_annotate.py --annot fig3a_annot.txt Fig3a.xml Hydrolases_branchthickness.xml
+python2 graphlan.py Hydrolases_branchthickness.xml Fig3a_phylotree.png --dpi 600 --size 13
